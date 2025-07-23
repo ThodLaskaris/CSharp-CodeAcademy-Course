4ee6d00ddef29b28dfc32d0228794cc0e360350f
thodlaskaris <thodlas@icloud.com>
2025-07-23 03:54:48 +0300

    Enum Project

--- EnumProject/Program.cs
+++ EnumProject/Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Transform the expense approval process as part of the Finance Technology team! 
+Build a digital tracking system using C# enums to categorize, validate, and streamline submissions from draft to approval, redefining corporate expense management.
+Code review available when you’re done
+*/
+using System;
+
+class Program
+{
+  [Flags]
+  public enum ExpenseType
+  {
+    None = 0,
+
+    Travel = 1,
+    Meals = 2,
+    OfficeSupplies = 4,
+    Software = 8,
+    Entertainment = 16
+  }
+  static void Main(string[] args)
+  {
+    // Printing all the Expesne Types.
+    foreach (ExpenseType type in Enum.GetValues(typeof(ExpenseType)))
+    {
+      if (type != ExpenseType.None)
+      {
+        Console.WriteLine($"Expense Type: {type}");
+      }
+    }
+
+    ExpenseType expenses = ExpenseType.Travel | ExpenseType.Meals;
+    Console.WriteLine($"\nCombined Expenses: {expenses}");
+
+    if ((expenses & ExpenseType.Travel) == ExpenseType.Travel)
+    {
+      Console.WriteLine($"Travel expesnes are included.");
+    }
+    else
+    {
+      Console.WriteLine("Travel expenses are not included.");
+    }
+    Console.WriteLine(expenses);
+    expenses &= ~ExpenseType.Meals;
+    Console.WriteLine(expenses);
+
+    // Parse a string to enum.
+
+    string passingExpense = "Travel|Software";
+    if (Enum.TryParse<ExpenseType>(passingExpense, ignoreCase: true, out ExpenseType parsedExpense))
+    {
+      Console.WriteLine($"Parsed Expense Type: {parsedExpense}");
+
+    }
+    else
+    {
+      Console.WriteLine("Invalid expesne type string.");
+    }
+
+  }
+}
